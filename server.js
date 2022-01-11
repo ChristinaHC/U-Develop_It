b85f1f7f748907eadb657e92b94d6bd629969a31
Christina Crabtree <borrowedandblue@gmail.com>
2022-01-11 17:47:12 -0500

    adding queries

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
+const { isBuffer } = require("util");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -18,8 +19,37 @@ const db = mysql.createConnection(
     console.log("Connected to the election database.")
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// Get all candidates and their information
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// Get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 12`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 // Default response for any other request (Not Found)
